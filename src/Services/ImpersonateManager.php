67f873210ebef900c7617f036c67b91fc4d31177
benderonlinesup <48205138+benderonlinesup@users.noreply.github.com>
2019-07-25 15:49:36 +0300

    5.8 fix

--- src/Services/ImpersonateManager.php
+++ src/Services/ImpersonateManager.php
@@ -103,7 +103,7 @@ class ImpersonateManager
             return false;
         }
 
-        $this->app['events']->fire(new TakeImpersonation($from, $to));
+        $this->app['events']->dispatch(new TakeImpersonation($from, $to));
 
         return true;
     }
@@ -127,7 +127,7 @@ class ImpersonateManager
             return false;
         }
 
-        $this->app['events']->fire(new LeaveImpersonation($impersonator, $impersonated));
+        $this->app['events']->dispatch(new LeaveImpersonation($impersonator, $impersonated));
 
         return true;
     }
